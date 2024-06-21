50af26a9477b2a6ac99a1c74c0f62affd7979c4f
BrucePang <brucepang0618@gmail.com>
2024-06-21 10:16:03 +0800

    [feature]: bak urls.py on 20240621

---
+++ tutorial/urls_old20240618.py
@@ -0,0 +1,36 @@
+"""
+URL configuration for tutorial project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/4.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.contrib import admin
+from django.urls import path
+from quikstart import views
+
+urlpatterns = [
+    # path('admin/', admin.site.urls),
+    path('auth/', views.auth), # FBV: function based view -> 请求进来 函数()
+
+    # /user/123/ => pk=123
+    path('user/<int:pk>/', views.UserView.as_view()), # CBV: class based view -> 请求进来 函数()
+
+
+    # /info/xxxxx/ => dt=xxxxx
+    path('info/<str:dt>/', views.InfoView.as_view()), # CBV: class based view -> 请求进来 函数()
+
+    path('login/', views.LoginView.as_view()),
+    path('user/', views.UserView.as_view()),
+    path('order/', views.OrderView.as_view()),
+
+]
