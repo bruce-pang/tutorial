37286154583cd54abffc98e2915bc08c48eb9e5f
brucepang <brucepang0618@gmail.com>
2024-06-23 16:58:39 +0800

    [feature]: serializable demo

--- serializer_quik/models.py
+++ serializer_quik/models.py
@@ -6,6 +6,9 @@ class Depart(models.Model):
     order = models.IntegerField(verbose_name='排序', default=0)
     count = models.IntegerField(verbose_name='人数', default=0)
 
+class Tag(models.Model):
+    caption = models.CharField(verbose_name='标签', max_length=64)
+
 
 class User(models.Model):
     name = models.CharField(max_length=20)
@@ -14,3 +17,5 @@ class User(models.Model):
     depart = models.ForeignKey(verbose_name='部门',to='Depart', on_delete=models.CASCADE) # 外键关联， on_delete=models.CASCADE表示删除关联数据时，关联数据也删除
     createtime = models.DateTimeField(verbose_name='创建时间',auto_now_add=True) # 创建时间
     updatetime = models.DateTimeField(verbose_name='更新时间',  default=timezone.now) # 更新时间
+
+    tags = models.ManyToManyField(verbose_name='标签',to='Tag') # 多对多关联, 会自动创建中间表
--- serializer_quik/views.py
+++ serializer_quik/views.py
@@ -6,7 +6,7 @@ from django.utils import timezone
 from rest_framework.response import Response
 from rest_framework.views import APIView
 from rest_framework import serializers
-from serializer_quik.models import Depart, User
+from serializer_quik.models import Depart, User, Tag
 
 
 # 由于json.dumps()无法序列化datetime, decimal类型，所以需要使用DRF的序列化器
@@ -15,21 +15,48 @@ from serializer_quik.models import Depart, User
 #    order = serializers.IntegerField()
 #    count = serializers.IntegerField()
 
+class BaseSerializer(serializers.Serializer):
+    xx = serializers.CharField(source="name")
+
 class DepartSerializer(serializers.ModelSerializer): # 多字段建议使用ModelSerializer
     class Meta:
         model = Depart
         fields = "__all__"
 
-class UserSerializer(serializers.ModelSerializer):
-    # ctime = serializers.DateTimeField(format="%Y-%m-%d", source="createtime")
+class D1(serializers.ModelSerializer):
+    class Meta:
+        model = Depart
+        fields = ["title", "order"]
+
+class TagsSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Tag
+        fields = "__all__"
+
+class UserSerializer(serializers.ModelSerializer, BaseSerializer):
+    ctime = serializers.DateTimeField(format="%Y-%m-%d", source="createtime")
     #ctime = serializers.DateTimeField(format="%Y-%m-%d", source="updatetime")
     gender = serializers.CharField(source="get_gender_display") # 通过get_字段名_display()方法获取choices的值
-    depart = serializers.CharField(source="depart.title")
-    age = serializers.IntegerField()
+    # depart = serializers.CharField(source="depart.title")
+    # age = serializers.IntegerField()
+    # xxx = serializers.SerializerMethodField() # 自定义你想要显示的数据
+    # tags = serializers.SerializerMethodField()
+
+    depart = D1()
+    tags = TagsSerializer(many=True) # 多对多关联，需要指定many=True
+
     class Meta:
         model = User
         # fields = "__all__" # 正常被序列化
-        fields = ["name", "age", "gender","depart", "createtime"] # 指定序列化的字段
+        fields = ["name", "age", "gender","depart", "ctime", "xx", "tags"] # 指定序列化的字段
+    def get_xxx(self, obj):
+        return "{}-{}".format(obj.name, obj.age)
+
+    def get_tags(self, obj):
+        # [Tag对象, Tag对象, Tag对象]
+        tag_list = obj.tags.all()
+        print(tag_list)
+        return [ {"id": row.id, "caption": row.caption} for row in tag_list]
 
 class DepartView(APIView):
     authentication_classes = []
